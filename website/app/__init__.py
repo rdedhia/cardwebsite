da9c8acc03109a309997763e473014460c901b05
deborahhellen <hellen.deborahv@gmail.com>
2013-11-04 17:50:04 -0500

    Added website folder which includes the virtual environment for flask and template for website

---
+++ website/app/__init__.py
@@ -0,0 +1,4 @@
+from flask import Flask, url_for
+ 
+app = Flask(__name__)
+from app import controllers
---
+++ website/run.py
@@ -0,0 +1,7 @@
+#!flask/bin/python
+from app import app
+app.run(debug = True)
+
+#from the terminal run by navigating into Github\cardwebsite\testapp then type 
+#>flask\Scripts\python.exe run.py
+#go to localhost:5000 in your browser to view
